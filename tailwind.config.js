aacde255a7a6b909dcd923b0cbd12b0dbb0a353f
Iamdodo22 <asimiazeez493@gail.com>
2023-09-21 16:58:12 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily:{
+        fBody:['Inter'],
+        fHead:['Prata']
+      }
+    },
+  },
+  plugins: [],
+}
+
